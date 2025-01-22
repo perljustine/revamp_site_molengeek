6626057c95acc8c1203ca7e7fe9a06dcbde488ea
perljustine <justine.perl@hotmail.com>
2025-01-22 14:03:01 +0100

    bruger js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Burger Menu
-    const burger = document.querySelector(".burger")
-    const nav = document.querySelector("nav ul")
-  
-    burger.addEventListener("click", () => {
-      nav.classList.toggle("show")
-      burger.classList.toggle("toggle")
-    });
+  // Burger Menu
+  const burger = document.querySelector(".burger")
+  const nav = document.querySelector("nav ul")
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("show")
+    burger.classList.toggle("toggle")
+  })
 });
+
+
+// Carousel 
+const images = document.querySelectorAll(".carousel img");
+const prevButton = document.querySelector(".carousel-nav.prev");
+const nextButton = document.querySelector(".carousel-nav.next");
+let currentIndex = 0;
