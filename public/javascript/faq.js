5c5397d3f786097afe12c099e8676c1ac6593ba3
perljustine <justine.perl@hotmail.com>
2025-01-23 21:06:20 +0100

    style faq

--- public/javascript/faq.js
+++ public/javascript/faq.js
@@ -2,8 +2,9 @@ const faqButtons = document.querySelectorAll('.faq-btn'); // Sélectionner tous 
 
 faqButtons.forEach(button => { // Parcourir chaque bouton de la list
     button.addEventListener('click', () => { // quand on clique la fonction se réalise
-        const details = button.nextElementSibling; // Récupérer le contenu qui est après le bouton dans le html 
-        details.classList.toggle('visible'); // Afficher ou cacher le contenu de la classe visible ajoutée au clique si elle l'est elle est supp sinon elle est ajoutée
-        button.textContent = details.classList.contains('visible') ? '-' : '+'; // Mettre à jour le texte du bouton : true = ? false = : 
+        const faqText = button.parentElement.nextElementSibling; 
+        faqText.classList.toggle('visible');
+        // Afficher ou cacher le contenu de la classe visible ajoutée au clique si elle l'est elle est supp sinon elle est ajoutée
+        button.textContent = faqText.classList.contains('visible') ? '-' : '+'; // Mettre à jour le texte du bouton : true = ? false = : 
     });
 });
